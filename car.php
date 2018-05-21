5b150a3744e210522a7149b32b007f7243d42dd3
Michał Łupiński <michal.lupinski121@gmail.com>
2018-05-21 20:05:58 +0200

    Prawie Gotowe

---
+++ car.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'function.php';
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+	<head>
+		<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css" type="text/css" />
+		<link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">
+	</head>
+	<body>
+		<div class="container con2">
+        <?php show_car_info(); ?>
+  	</div>
+	</body>
+</html>
